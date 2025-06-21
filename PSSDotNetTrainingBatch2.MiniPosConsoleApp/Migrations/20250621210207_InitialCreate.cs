fcb2599be4f188302aaf3a391cbaf8a27abac425
Pau Sian Sang <paungshinsan27@gmail.com>
2025-06-22 04:59:28 +0630

    feat: run migrations and create productservices

---
+++ PSSDotNetTrainingBatch2.MiniPosConsoleApp/Migrations/20250621210207_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSSDotNetTrainingBatch2.MiniPosConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tbl_Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tbl_Sale",
+                columns: table => new
+                {
+                    SaleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SaleDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VoucherNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_Sale", x => x.SaleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tbl_SaleDetail",
+                columns: table => new
+                {
+                    SaleDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SaleId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_SaleDetail", x => x.SaleDetailId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tbl_Product");
+
+            migrationBuilder.DropTable(
+                name: "Tbl_Sale");
+
+            migrationBuilder.DropTable(
+                name: "Tbl_SaleDetail");
+        }
+    }
+}
---
+++ PSSDotNetTrainingBatch2.MiniPosConsoleApp/Migrations/20250621212229_AddDeleteFlagColumnToAllModels.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSSDotNetTrainingBatch2.MiniPosConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeleteFlagColumnToAllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DeleteFlag",
+                table: "Tbl_SaleDetail",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DeleteFlag",
+                table: "Tbl_Sale",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DeleteFlag",
+                table: "Tbl_Product",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteFlag",
+                table: "Tbl_SaleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteFlag",
+                table: "Tbl_Sale");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteFlag",
+                table: "Tbl_Product");
+        }
+    }
+}
