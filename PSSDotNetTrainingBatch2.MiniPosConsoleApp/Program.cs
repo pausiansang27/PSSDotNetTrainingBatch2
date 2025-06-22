934d003ced17abd31cf989b5b05e041367d573c6
Pau Sian Sang <paungshinsan27@gmail.com>
2025-06-22 17:01:03 +0630

    feat: add update and delete logic to productservice

--- PSSDotNetTrainingBatch2.MiniPosConsoleApp/Program.cs
+++ PSSDotNetTrainingBatch2.MiniPosConsoleApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PSSDotNetTrainingBatch2.MiniPosConsoleApp.Services;
+
 Console.WriteLine("MINI POS");
+
+ProductService productService = new ProductService();
+//productService.Read();
+//productService.Edit();
+//productService.Create();
+productService.Update();
+//productService.Delete();
---
+++ PSSDotNetTrainingBatch2.MiniPosConsoleApp/Services/ProductService.cs
@@ -0,0 +1,169 @@
+﻿using PSSDotNetTrainingBatch2.MiniPosConsoleApp.Models;
+
+namespace PSSDotNetTrainingBatch2.MiniPosConsoleApp.Services
+{
+    public class ProductService
+    {
+        public void Read()
+        {
+            Console.WriteLine("Retrive All Products \n");
+            AppDbContext db = new AppDbContext();
+            db.Products.Where(p => !p.DeleteFlag).ToList().ForEach(DisplayProductDetail);
+        }
+
+        public void Edit()
+        {
+        FindProductById:
+            Console.WriteLine("Edit Product by Id \n");
+            AppDbContext db = new AppDbContext();
+            int productId = GetProductIdFromUser();
+            Product? product = GetProductById(productId, db);
+            if (product is null)
+            {
+                Console.WriteLine("Product not found for Product Id: " + productId);
+                goto FindProductById;
+            }
+            DisplayProductDetail(product);
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Create Product \n");
+            string name = GetProductNameFromUser();
+            decimal price = GetProductPriceFromUser();
+            int quantity = GetProductQuantityFromUser();
+
+            Product product = new Product()
+            {
+                Name = name,
+                Price = price,
+                Quantity = quantity
+            };
+
+            AppDbContext db = new AppDbContext();
+            db.Products.Add(product);
+            int result = db.SaveChanges();
+            Console.WriteLine(result > 0 ? "Product creation successful!" : "Product creation failed!");
+        }
+
+        public void Update()
+        {
+        FindProductById:
+            Console.WriteLine("Update Product by Id \n");
+            AppDbContext db = new AppDbContext();
+            int productId = GetProductIdFromUser();
+            Product? product = GetProductById(productId, db);
+            if (product is null)
+            {
+                Console.WriteLine("Product not found for Product Id: " + productId);
+                goto FindProductById;
+            }
+
+            DisplayProductDetail(product);
+            string name = GetProductNameFromUser();
+            decimal price = GetProductPriceFromUser();
+            int quantity = GetProductQuantityFromUser();
+
+            product.Name = name;
+            product.Price = price;
+            product.Quantity = quantity;
+
+            int result = db.SaveChanges();
+            Console.WriteLine(result > 0 ? "Updating product success!" : "Update Failed!");
+        }
+
+        public void Delete()
+        {
+        FindProductById:
+            Console.WriteLine("Delete Product by Id \n");
+            AppDbContext db = new AppDbContext();
+            int productId = GetProductIdFromUser();
+            Product? product = GetProductById(productId, db);
+            if (product is null)
+            {
+                Console.WriteLine("Product not found for Product Id: " + productId);
+                goto FindProductById;
+            }
+
+            DisplayProductDetail(product);
+            product.DeleteFlag = true;
+            int result = db.SaveChanges();
+            Console.WriteLine(result > 0 ? "Product deletion successful!" : "Product deletion Failed!");
+        }
+
+        public static void DisplayProductDetail(Product product)
+        {
+            Console.WriteLine("Product Id => " + product.ProductId);
+            Console.WriteLine("Product name => " + product.Name);
+            Console.WriteLine("Product Price => " + product.Price);
+            Console.WriteLine("Quantity => " + product.Quantity + "\n");
+        }
+
+        public static int GetProductIdFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Enter Product Id: ");
+                string input = Console.ReadLine()!;
+                bool isInteger = int.TryParse(input, out int productId);
+                if (!isInteger)
+                {
+                    Console.WriteLine("Invalid Id. Please enter a valid Id!");
+                    continue;
+                }
+                return productId;
+            }
+        }
+
+        public static string GetProductNameFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Enter Product Name: ");
+                string name = Console.ReadLine()!;
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("Product name cannot be empty!");
+                    continue;
+                }
+                return name;
+            }
+        }
+
+        public static decimal GetProductPriceFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Enter Product Price: ");
+                string input = Console.ReadLine()!;
+                if (string.IsNullOrWhiteSpace(input) || !decimal.TryParse(input, out decimal price))
+                {
+                    Console.WriteLine("Please enter a valid price!");
+                    continue;
+                }
+                return price;
+            }
+        }
+
+        public static int GetProductQuantityFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Enter Product Quantity: ");
+                string input = Console.ReadLine()!;
+                if (string.IsNullOrWhiteSpace(input) || !int.TryParse(input, out int quantity))
+                {
+                    Console.WriteLine("Please enter a valid quantity!");
+                    continue;
+                }
+                return quantity;
+            }
+        }
+
+        public static Product? GetProductById(int id, AppDbContext db)
+        {
+            Product? product = db.Products.FirstOrDefault(p => p.ProductId == id && !p.DeleteFlag);
+            return product;
+        }
+    }
+}
