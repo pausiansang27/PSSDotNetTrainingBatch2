6468c172de08cac83a14230df3d5307ae2ddd004
Pau Sian Sang <paungshinsan27@gmail.com>
2025-07-26 16:46:21 +0630

    feat: add form UI and actions

---
+++ PSSDotNetTrainingBatch2.WinFormsApp1/FrmLogin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PSSDotNetTrainingBatch2.WinFormsApp1
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            btnLogin = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(121, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(121, 100);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(231, 23);
+            txtUsername.TabIndex = 1;
+            txtUsername.Text = "S001";
+            txtUsername.KeyDown += txtUsername_KeyDown;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(121, 163);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(231, 23);
+            txtPassword.TabIndex = 3;
+            txtPassword.Text = "1234";
+            txtPassword.UseSystemPasswordChar = true;
+            txtPassword.KeyDown += txtPassword_KeyDown;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(121, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(277, 205);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 30);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "&Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(462, 333);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(label2);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Name = "FrmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Label label2;
+        private Button btnLogin;
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ PSSDotNetTrainingBatch2.WinFormsApp1/FrmLogin.cs
@@ -0,0 +1,46 @@
+﻿using PSSDotNetTrainingBatch2.WinFormsApp1.Database.AppDbContextModels;
+
+namespace PSSDotNetTrainingBatch2.WinFormsApp1
+{
+    public partial class FrmLogin : Form
+    {
+        private readonly AppDbContext _db;
+        public FrmLogin()
+        {
+            InitializeComponent();
+            _db = new AppDbContext();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string staffCode = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            var item = _db.TblStaffs.FirstOrDefault(x => x.StaffCode == staffCode && x.Password == password);
+            if (item == null)
+            {
+                MessageBox.Show("Username or Password was wrong!", "Authentication Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            this.Hide();
+            FrmMenu frmMenu = new FrmMenu();
+            frmMenu.ShowDialog();
+            this.Show();
+        }
+
+        private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtPassword.Focus();
+            }
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLogin_Click(sender, e);
+            }
+        }
+    }
+}
